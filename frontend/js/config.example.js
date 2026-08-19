3679d61c461a3af94a67a90431ed51fad1989add
Ibad Shaikh <ibad84671@gmail.com>
2026-08-19 13:11:54 +0530

    add frontend configuration example

---
+++ frontend/js/config.example.js
@@ -0,0 +1,7 @@
+// Copy this to config.js for local static development.
+window.CLOUDDROP_CONFIG = {
+  apiBase: 'https://YOUR_API_ID.execute-api.YOUR_REGION.amazonaws.com/prod',
+  region: 'us-east-1',
+  userPoolId: 'YOUR_USER_POOL_ID',
+  clientId: 'YOUR_COGNITO_APP_CLIENT_ID'
+};
