d7ac210350998bcdfa7c615871f5ea1ca99f9125
Ibad Shaikh <ibad84671@gmail.com>
2026-08-19 18:56:57 +0530

    fix: align frontend runtime configuration with deployment outputs

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,6 +1,8 @@
-window.CloudDropConfig = { 
-  API_BASE: '%API_URL%', 
-  COGNITO_DOMAIN: '%DOMAIN%', 
-  COGNITO_CLIENT_ID: '%CLIENT_ID%', 
-  TRANSFER_DAYS: 7 
-}; 
+window.CLOUDDROP_CONFIG = {
+  apiBase: '',
+  region: '',
+  cognitoClientId: '',
+  cognitoUserPoolId: '',
+  cognitoDomain: '',
+  emailEnabled: false
+};
